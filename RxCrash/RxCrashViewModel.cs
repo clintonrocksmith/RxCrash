1fd328c95d781fab21f80236f8c5ddf7b43a18d4
Clinton Rocksmith <clinton@rocksmithtech.com>
2020-01-17 07:43:33 +1000

    Added demonstration solution

---
+++ RxCrash/RxCrashViewModel.cs
@@ -0,0 +1,16 @@
+﻿using ReactiveUI;
+
+namespace RxCrash
+{
+    public class RxCrashViewModel : ReactiveObject
+    {
+        private string _someText;
+
+        public string SomeText
+        {
+            get => _someText;
+            set => this.RaiseAndSetIfChanged(ref _someText, value);
+        }
+    }
+}
+
