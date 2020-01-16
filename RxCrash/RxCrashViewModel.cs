b549e50e7542ebb080fc186aaded3967390b9b5a
Clinton Rocksmith <clinton@rocksmithtech.com>
2020-01-17 07:48:43 +1000

    Update RxCrashViewModel.cs

--- RxCrash/RxCrashViewModel.cs
+++ RxCrash/RxCrashViewModel.cs
@@ -4,6 +4,11 @@ namespace RxCrash
 {
     public class RxCrashViewModel : ReactiveObject
     {
+        public RxCrashViewModel()
+        {
+            SomeText = "It works!";
+        }
+        
         private string _someText;
 
         public string SomeText
